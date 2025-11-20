30217dba24d21f1bc6ffb38a4ee48b0233106068
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-11-20 16:29:25 +0200

    feat: added SocialMediaAuth service

---
+++ lib/src/core/services/social_media_auth_service.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../providers/supabase_providers.dart';
+import '../supabase/supabase_error_message.dart';
+
+final googleSignInServiceProvider =
+    Provider.autoDispose<SocialMediaAuthService>(
+      (ref) => SocialMediaAuthServiceImpl(ref.read(supabaseProvider)),
+    );
+
+abstract class SocialMediaAuthService {
+  Future<void> googleSignIn();
+  Future<void> facebookSignIn();
+}
+
+class SocialMediaAuthServiceImpl implements SocialMediaAuthService {
+  final SupabaseClient _supabaseClient;
+
+  SocialMediaAuthServiceImpl(this._supabaseClient);
+
+  @override
+  Future<void> googleSignIn() async {
+    if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+      await _nativeGoogleSignIn();
+    }
+  }
+
+  Future<void> _nativeGoogleSignIn() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId:
+          '39485904573-b7ituag7p10ifv3ibca6f6tbrkr28rcs.apps.googleusercontent.com',
+      serverClientId:
+          '39485904573-bjlsp267odll3cjl4tldkhhhbdr8qrlc.apps.googleusercontent.com',
+    );
+
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser?.authentication;
+    final accessToken = googleAuth?.accessToken;
+    final idToken = googleAuth?.idToken;
+
+    if (accessToken == null) {
+      throw SupabaseErrorMessage.noAccessTokenFound;
+    }
+    if (idToken == null) {
+      throw SupabaseErrorMessage.noIdTokenFound;
+    }
+    await _supabaseClient.auth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+  }
+
+  @override
+  Future<void> facebookSignIn() {
+    return _supabaseClient.auth.signInWithOAuth(
+      OAuthProvider.facebook,
+      authScreenLaunchMode: LaunchMode.inAppBrowserView,
+    );
+  }
+}
